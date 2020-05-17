7cb3868d3403658e8f5362151618439383bd27e0
unknown <fkusenda@W0GL2005.beeone.lan>
2020-05-17 21:52:37 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+   selector: 'app-root',
+   template: `
+				<app-article></app-article>
+             `
 })
-export class AppComponent {
-  title = 'angular-demo';
+export class AppComponent { 
 }
+    
